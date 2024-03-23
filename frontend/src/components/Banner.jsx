6540e35a059a29879b7fffbec8a5ff39332b9335
Hoang Nguyen <ndhoang129@gmail.com>
2024-03-23 16:42:02 +0700

    Create banner and header

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,26 @@
+export default function Banner(props) {
+    const title = props.title || <>Default <br />Kit</>;
+    const description = props.description || "Default description";// "Ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"; // props.description;
+    return (
+        <div className="banner_section layout_padding">
+            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-sm-6">
+                                    <h1 className="banner_taital">{title}</h1>
+                                    <p className="banner_text">{description}</p>
+                                    <div className="read_bt"><a href="#">Buy Now</a></div>
+                                </div>
+                                <div className="col-sm-6">
+                                    <div className="banner_img"><img src="images/banner-img.png" /></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
