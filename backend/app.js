2852fd95057f9a2c15eb5b03389950d5208ea6b1
ndhoang129 <ndhoang129@gmail.com>
2024-03-31 14:05:12 +0700

    Add GET /banners api

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,27 @@ app.get('/health', (req, res) => {
     });
 });
 
+app.get('/banners', (req, res) => {
+    const banners = [
+        {
+            title: "Makeup <br />Kit 1",
+            description: "Ncididunt 1 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+            image: "/images/banner-img.png"
+        },
+        {
+            title: "Makeup <br />Kit 2",
+            description: "Ncididunt 2 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+            image: "/images/banner-img.png"
+        },
+        {
+            title: "Makeup <br />Kit 3",
+            description: "Ncididunt 3 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
+            image: "/images/banner-img.png"
+        }
+    ];
+    res.send(banners);
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
