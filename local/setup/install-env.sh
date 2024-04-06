2e39bccd4eb6ce2071b0bff0e2d3a4f9ff41e433
ndhoang129 <ndhoang129@gmail.com>
2024-04-06 15:00:05 +0700

    Add provision script to run backend on vagrant vm

--- local/setup/install-env.sh
+++ local/setup/install-env.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+# installs NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# export nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# download and install Node.js
+nvm install 20
+# install pm2
+npm install pm2@latest -g
+# run backend
+cd /vagrant/backend
+pm2 start app.js
+
 sudo apt update
 sudo apt install -y nginx
 sudo cp /vagrant/default /etc/nginx/sites-enabled/default
